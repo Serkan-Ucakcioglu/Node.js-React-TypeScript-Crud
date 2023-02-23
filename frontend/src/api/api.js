e95500cae018cd1cb6e497bc267019cf3b844dac
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-23 16:43:31 +0300

    getAllUser function create and logo delete

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,3 +3,8 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "http://localhost:3002/",
 });
+
+export const getAllUser = async () => {
+  const { data } = await api.get("/users");
+  return data;
+};
