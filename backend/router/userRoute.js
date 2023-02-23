ae5d3cd162673973a32c4a6d15cbbe6ee016ea60
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-23 16:03:26 +0300

    route add getAllUser

--- backend/router/userRoute.js
+++ backend/router/userRoute.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const { getAllUser } = require("../controller/userController");
 
 const router = express.Router();
+
+router.get("/users", getAllUser);
 
 module.exports = router;
