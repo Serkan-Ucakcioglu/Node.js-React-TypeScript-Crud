186927da05d958b7635ab300c0aef52c24f1ff13
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-24 10:30:46 +0300

    edit svg add

--- frontend/src/Components/Card.tsx
+++ frontend/src/Components/Card.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import EditSvg from "../assets/EditSvg";
 import TrashSvg from "../assets/TrashSvg";
 import { UserList } from "./Home";
 
@@ -9,7 +10,10 @@ interface users {
 function Card({ user }: users) {
   return (
     <div className="flex flex-col gap-y-3 items-center p-2 justify-start h-[160px] w-[220px] border border-gray-400 rounded">
-      <span>{user.name}</span>
+      <div className="flex flex-row items-center">
+        <span>{user.name}</span>
+        <EditSvg />
+      </div>
       <span>{user.email}</span>
       <span>{user.title}</span>
       <span className="w-full flex justify-center items-center">
