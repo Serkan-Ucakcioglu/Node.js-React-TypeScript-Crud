37004ec7fc7a94038ab1ac2d93a290c248fd27de
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-24 13:05:19 +0300

    name error message

--- frontend/src/Components/Modal/InputList.tsx
+++ frontend/src/Components/Modal/InputList.tsx
@@ -19,8 +19,10 @@ function InputList({ register, errors }: any) {
           })}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
           placeholder="John Doe"
-          required
         />
+        <span className="text-red-500 text-left text-sm text-black">
+          {errors?.name?.message}
+        </span>
       </div>
       <div>
         <label className="block text-left mb-2 text-sm font-medium text-gray-900 dark:text-white">
