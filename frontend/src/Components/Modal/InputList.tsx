7a91645db7f9bdfc860b9bfef005519ccc1036cd
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-24 13:07:13 +0300

    update

--- frontend/src/Components/Modal/InputList.tsx
+++ frontend/src/Components/Modal/InputList.tsx
@@ -33,11 +33,16 @@ function InputList({ register, errors }: any) {
           Your Email
         </label>
         <input
+          {...register("email", {
+            required: "required!",
+          })}
           type="email"
           placeholder="serkanuckc123@gmail.com"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-          required
         />
+        <span className="text-red-500 text-left text-sm text-black">
+          {errors?.email?.message}
+        </span>
       </div>
       <div>
         <label className="block text-left mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -45,9 +50,23 @@ function InputList({ register, errors }: any) {
         </label>
         <input
           type="title"
+          {...register("title", {
+            required: "required!",
+            minLength: {
+              value: 8,
+              message: "Minumum Length 8!",
+            },
+            maxLength: {
+              value: 40,
+              message: "Maximum Length 40!",
+            },
+          })}
           placeholder="Hello World !"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
         />
+        <span className="text-red-500 text-left text-sm text-black">
+          {errors?.title?.message}
+        </span>
       </div>
     </>
   );
