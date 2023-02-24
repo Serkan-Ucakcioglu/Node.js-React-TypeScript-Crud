185e4fc8beee206525da8a3adff9722505ae6e6a
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-24 13:05:57 +0300

    maxLength add

--- frontend/src/Components/Modal/InputList.tsx
+++ frontend/src/Components/Modal/InputList.tsx
@@ -16,6 +16,10 @@ function InputList({ register, errors }: any) {
               value: 3,
               message: "Minumum Length 3!",
             },
+            maxLength: {
+              value: 15,
+              message: "Maximum Length 15!",
+            },
           })}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
           placeholder="John Doe"
