96d6a896467b0fcaa4216fe4743ed4866a7e7b11
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-03-09 11:19:26 +0300

    delete svg add

--- frontend/src/Components/Modal/DeleteModal.tsx
+++ frontend/src/Components/Modal/DeleteModal.tsx
@@ -3,6 +3,7 @@ import TrashSvg from "../../assets/TrashSvg";
 import useShowModel from "../../hooks/useShowModel";
 import { useSWRConfig } from "swr";
 import { deleteUser } from "../../api/api";
+import Delete from "../../assets/Delete";
 
 type ID = { id: String };
 
@@ -38,21 +39,7 @@ function DeleteModal({ id }: ID) {
                   <span className="sr-only">Close modal</span>
                 </button>
                 <div className="p-6 text-center">
-                  <svg
-                    aria-hidden="true"
-                    className="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                    ></path>
-                  </svg>
+                  <Delete />
                   <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
                     Delete User ?
                   </h3>
