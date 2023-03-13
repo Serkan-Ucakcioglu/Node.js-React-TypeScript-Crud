9f59eec315e9de2634a2084ee413f43bd0a6627a
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-03-13 11:49:13 +0300

    optional chaning add

--- frontend/src/Components/Card.tsx
+++ frontend/src/Components/Card.tsx
@@ -9,11 +9,11 @@ function Card({ user }: users) {
   return (
     <div className="flex flex-col gap-y-3 items-center p-3 justify-start h-[180px] w-[225px] border border-gray-400 rounded">
       <div className="flex flex-row items-center">
-        <span>{user.name}</span>
+        <span>{user?.name}</span>
         <EditModal user={user} />
       </div>
-      <span>{user.email}</span>
-      <span>{user.title}</span>
+      <span>{user?.email}</span>
+      <span>{user?.title}</span>
       <DeleteModal id={user?._id} />
     </div>
   );
