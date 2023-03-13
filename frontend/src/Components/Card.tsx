06bd234f21ffa00a381e4fe0ed07dc4bc5bc2a40
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-03-13 11:57:30 +0300

    gap add

--- frontend/src/Components/Card.tsx
+++ frontend/src/Components/Card.tsx
@@ -8,7 +8,7 @@ export interface users {
 function Card({ user }: users) {
   return (
     <div className="flex flex-col gap-y-3 items-center p-3 justify-start h-[180px] w-[225px] border border-gray-400 rounded">
-      <div className="flex flex-row items-center">
+      <div className="flex flex-row gap-2 items-center">
         <span>{user?.name}</span>
         <EditModal user={user} />
       </div>
