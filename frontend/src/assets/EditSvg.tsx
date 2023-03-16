1cdd9baff35c3d7a73c36c24768c69a4015c02af
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-03-16 15:40:42 +0300

    scale add

--- frontend/src/assets/EditSvg.tsx
+++ frontend/src/assets/EditSvg.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function EditSvg() {
   return (
     <svg
-      className="w-6 h-[30px]"
+      className="w-6 h-[30px] hover:scale-125"
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
