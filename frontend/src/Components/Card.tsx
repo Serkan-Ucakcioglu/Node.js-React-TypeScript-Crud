6f480c9f3915e48654640e45e2ea400268a4e0e9
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-03-16 15:30:08 +0300

    hover border and shadow add

--- frontend/src/Components/Card.tsx
+++ frontend/src/Components/Card.tsx
@@ -7,7 +7,7 @@ export interface users {
 
 function Card({ user }: users) {
   return (
-    <div className="flex flex-col gap-y-3 items-center p-3 justify-start h-[180px] w-[225px] border border-gray-400 rounded">
+    <div className="flex shadow flex-col gap-y-3 items-center p-3 justify-start h-[180px] w-[225px] border border-gray-400 hover:border-gray-700 rounded">
       <div className="flex flex-row gap-2 items-center">
         <span>{user?.name}</span>
         <EditModal user={user} />
